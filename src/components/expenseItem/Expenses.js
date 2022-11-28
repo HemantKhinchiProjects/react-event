d484fb70c2140ef2f99339e9a25274bc998b4cbb
HemantTrellance <99024438+HemantTrellance@users.noreply.github.com>
2022-11-28 12:58:52 +0530

    1 file modified

--- src/components/expenseItem/Expenses.js
+++ src/components/expenseItem/Expenses.js
@@ -21,15 +21,18 @@ const Expenses = (props) => {
           selected={filtredYear}
           onChangeFilter={filterChangehandler}
         />
-        
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {filteredExpenses.length === 0 ? (
+          <p>No expenses Found </p>
+        ) : (
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))
+        )}
       </Card>
     </div>
   );
